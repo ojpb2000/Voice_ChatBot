c30591a04d2a5c3e1a441f1688433dac86596f1a
ojpb2000 <ojpb2000@gmail.com>
2025-09-25 19:31:11 -0600

    fix: move WebSocket import to top of file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const WebSocket = require('ws');
 require('dotenv').config();
 
 const app = express();
